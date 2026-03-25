3155af7c5c0063949cca8db1c1e33ae3bcac4929
Eduardo Carmona <eduardo10705316@edu.pa.senac.br>
2026-03-25 20:39:04 -0300

    exercícios de input

--- exercicio1.py
+++ exercicio1.py
@@ -2,3 +2,8 @@ num1 = float(input("Digite o primeiro número:  "))
 num2 = float(input("digite o segundo número: "))
 soma = num1 + num2
 print("A soma é" ,soma)
+a = 6
+b = 3
+a /= 2 * b
+print(a)
+ 
