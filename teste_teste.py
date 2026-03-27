9f81c375184488d02c944485a5440c00c4eee9d9
Eduardo Carmona <eduardo10705316@edu.pa.senac.br>
2026-03-27 20:56:05 -0300

    Update teste_teste.py

--- teste_teste.py
+++ teste_teste.py
@@ -3,10 +3,6 @@ valor = float(input("Insira um valor"))
 if valor < 85.528:
     tax = valor * 0.18 - 556.02
     print("Seu imposto calculado é", tax)
-elif valor > 85.528:
-    tax = valor * 0.32 + 14.839
-    print("A Taxa é", tax) 
 else:
-    tax = round(tax, 0)
-    print("A taxa é:", tax, "thalers") 
- 
+    tax = valor * 0.32 + 14.839
+    print("A Taxa é", round(tax,0)) 
